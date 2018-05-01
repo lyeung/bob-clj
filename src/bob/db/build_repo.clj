d5829c76fe915550dd2a8ca6f4cb9dfaf4929789
Lenming Yeung <lenming.yeung@gmail.com>
2018-05-01 23:52:11 +1000

    Added find-by-key function.

--- src/bob/db/build_repo.clj
+++ src/bob/db/build_repo.clj
@@ -1,21 +1,48 @@
 (ns bob.db.build-repo
-  (:require [bob.db.core :as dbcore]))
+  (:require
+   [bob.db.core :as dbcore]))
 
+;; keyname constant
 (def keyname "build-repo:")
 
+;; build-repo-key specs and fn
 (defn build-repo-key [build-repo]
-  (->> (:id build-repo)
+  (->> build-repo
+       (:id)
        (str keyname)))
+
+;; id-from-key
+(defn id-from-key [k]
+  (let [result (re-matches #"\S+:(.+)" k)]
+    (if (nil? result)
+      result
+      (second result))))
 
 (defn build-repo-subkey-vals [build-repo]
   (list "name" (:name build-repo)
    "url" (:url build-repo)))
 
 (defn build-repo-content [build-repo]
-  (flatten (list  (build-repo-key build-repo)
+  (flatten (list (build-repo-key build-repo)
         (build-repo-subkey-vals build-repo))))
 
 (defn save [build-repo]
   (let [args (build-repo-content build-repo)]
     (apply dbcore/save-hash args)))
 
+;; tuplize specs:w
+(defn tuplize [coll]
+  (loop [result []
+         c coll]
+    (if (empty? c)
+      result
+      (recur (conj result
+                   {(keyword (first c))
+                    (second c)})
+             (nthrest c 2)))))
+
+;; find specs
+(defn find-by-key [k]
+  (let [v (dbcore/find-hash k)]
+    (apply conj {:id (id-from-key k)}
+          (tuplize v))))
--- test/bob/db/build_repo_test.clj
+++ test/bob/db/build_repo_test.clj
@@ -9,14 +9,21 @@
 
 (def repoUrl "https://github.com/lyeung/bob-clj")
 
-(deftest test-build-repo
+(deftest test-helper-fns
   (let [build-repo
         {:id "hash123"
          :name "foo"
          :url repoUrl}]
+    (testing "tuplize"
+      (let [coll [:a 1 :b 2 :c 3 :d 4 :e 5]]
+        (is (= [{:a 1}, {:b 2}, {:c 3} {:d 4} {:e 5}]
+               (tuplize coll)))))
     (testing "build-repo key"
       (is (= "build-repo:hash123"
              (build-repo-key build-repo))))
+    (testing "id-from-key"
+      (is (= "hash123"
+             (id-from-key "build-repo:hash123"))))
     (testing "build-repo subkey vals"
       (is (= ["name"
               "foo"
@@ -25,11 +32,17 @@
              (build-repo-subkey-vals build-repo))))
     (testing "build-repo content"
       (is (= (list "build-repo:hash123"
-              "name"
-              "foo"
-              "url"
-              repoUrl)
-              (build-repo-content build-repo))))
+                   "name"
+                   "foo"
+                   "url"
+                   repoUrl)
+             (build-repo-content build-repo))))))
+
+(deftest test-build-repo
+  (let [build-repo
+        {:id "hash123"
+         :name "foo"
+         :url repoUrl}]
     (testing "save"
       (dbcore/remove-hash "build-repo:hash123"
                           "name")
@@ -45,4 +58,13 @@
               "url"
               repoUrl]
              (dbcore/find-hash
-              "build-repo:hash123"))))))
+              "build-repo:hash123"))))
+    (testing "find-by-key"
+      (dbcore/remove-hash "build-repo:hash123"
+                          "name")
+      (dbcore/remove-hash "build-repo:hash123"
+                          "url")
+      (save build-repo)
+       (is (= build-repo
+              (find-by-key "build-repo:hash123"))))
+      ))
